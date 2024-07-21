c8336d1100d0bbb4b3b4f5df29255412b4f2bd38
chaos-warzh <haidedeyu@163.com>
2024-07-21 14:57:46 +0800

    init

---
+++ src/main/java/github/chaoswarzh/icircle/IcircleApplication.java
@@ -0,0 +1,13 @@
+package github.chaoswarzh.icircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcircleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcircleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/chaoswarzh/icircle/IcircleApplicationTests.java
@@ -0,0 +1,13 @@
+package github.chaoswarzh.icircle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcircleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
