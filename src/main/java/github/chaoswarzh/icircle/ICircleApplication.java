2bd99e8d1e5620155c0860ad4b00c63611e39fba
chaos-warzh <haidedeyu@163.com>
2024-08-08 03:37:53 +0800

    +Project Entry

--- src/main/java/github/chaoswarzh/icircle/ICircleApplication.java
+++ src/main/java/github/chaoswarzh/icircle/ICircleApplication.java
@@ -2,12 +2,14 @@ package github.chaoswarzh.icircle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class IcircleApplication {
+@EnableScheduling
+public class ICircleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IcircleApplication.class, args);
+		SpringApplication.run(ICircleApplication.class, args);
 	}
 
 }
